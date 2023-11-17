dea0aac635adfa0c8e76474309dcba931286d21b
Etterbak <fijke.oei@gmail.com>
2023-11-17 16:08:17 +0100

    changed the code to include a command and options

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,14 +3,29 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
+@click.group()
+def smallangle():
+    pass
 
+@smallangle.command()
+@click.option(
+    "-n",
+    "--number",
+    type=int,
+    default=5,
+)
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
     return
 
-
+@smallangle.command()
+@click.option(
+    "-n",
+    "--number",
+    type=int,
+    default=5,)
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
@@ -19,4 +34,4 @@ def tan(number):
 
 
 if __name__ == "__main__":
-    sin(10)
+    smallangle()
