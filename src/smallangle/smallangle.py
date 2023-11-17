bdc597711b0a31af906ac88d5ded309d98c07d52
Etterbak <fijke.oei@gmail.com>
2023-11-17 16:35:35 +0100

    Update smallangle.py
    added docstrings

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -15,6 +15,11 @@ def smallangle():
     default=5,
 )
 def sin(number):
+    """Dataframe of sine wave.
+
+    Args:
+        number (int): number of steps between 0 and 2pi
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
@@ -27,6 +32,11 @@ def sin(number):
     type=int,
     default=5,)
 def tan(number):
+    """Dataframe of tangent wave.
+
+    Args:
+        number (int): number of steps between 0 and 2pi
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
